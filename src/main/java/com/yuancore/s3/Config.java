2f98ceab726e68188c2122e9f91c3d41e5e5a6bd
zhou.han <zhou.han@yuancore.com>
2022-03-03 16:12:53 +0800

    删除url信息

--- src/main/java/com/yuancore/s3/Config.java
+++ src/main/java/com/yuancore/s3/Config.java
@@ -3,7 +3,7 @@ package com.yuancore.s3;
 import com.yuancore.radosgw.sdk.RGWPassport;
 
 public class Config {
-    private final static String rgwUrl = "http://124.222.161.83:13000";
+    private final static String rgwUrl = "";
     private final static String userAccessKey = "aaa";
     private final static String userSecretKey = "bbb";
     public final static RGWPassport rgwPassport = new RGWPassport(rgwUrl, userAccessKey, userSecretKey);
