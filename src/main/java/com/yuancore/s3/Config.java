ab76c0250c9baaccc77ba93bbdf796ab5fa44c98
zhou.han <zhou.han@yuancore.com>
2022-03-24 14:16:23 +0800

    添加 s3Object.close();

--- src/main/java/com/yuancore/s3/Config.java
+++ src/main/java/com/yuancore/s3/Config.java
@@ -4,8 +4,8 @@ import com.yuancore.radosgw.sdk.RGWPassport;
 
 public class Config {
     private final static String rgwUrl = "";
-    private final static String userAccessKey = "aaa";
-    private final static String userSecretKey = "bbb";
+    private final static String userAccessKey = "111";
+    private final static String userSecretKey = "222";
     public final static RGWPassport rgwPassport = new RGWPassport(rgwUrl, userAccessKey, userSecretKey);
-    public final static String defaultBucketName = "bk1";
+    public final static String defaultBucketName = "bk3";
 }
--- src/main/java/com/yuancore/s3/Demo.java
+++ src/main/java/com/yuancore/s3/Demo.java
@@ -7,11 +7,13 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectResult;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.util.StringInputStream;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.UUID;
@@ -24,23 +26,30 @@ public class Demo {
         ClientConfiguration conf = new ClientConfiguration();
         conf.setProtocol(Protocol.HTTP);
         conf.setSignerOverride("S3SignerType");
+        conf.setMaxConnections(10); // 可以在此设置最大连接数
         AWSCredentials credentials = new BasicAWSCredentials(Config.rgwPassport.getAccessKey(),
                 Config.rgwPassport.getSecretKey());
 
         s3Client = new AmazonS3Client(credentials, conf);
         s3Client.setEndpoint(Config.rgwPassport.getEndPoint());
+
         System.out.println("初始化成功");
     }
 
     @Test
-    public void uploadObject() throws UnsupportedEncodingException {
+    public void uploadObject() throws IOException, InterruptedException {
         try {
 //            initClient();
             String objID = UUID.randomUUID().toString();
+            System.out.println(objID);
             ObjectMetadata metadata = new ObjectMetadata();
             metadata.addUserMetadata("x-ycore-filename", URLEncoder.encode("中文名", "UTF-8"));
-            s3Client.putObject(Config.defaultBucketName, objID, new StringInputStream("中文名需要url encode"), metadata);
-            S3Object obj = s3Client.getObject(Config.defaultBucketName, objID);
+            PutObjectResult r = s3Client.putObject(Config.defaultBucketName, objID, new StringInputStream("中文名需要url encode"), metadata);
+            for (int i = 0; i < 20; i++) {
+                S3Object obj = s3Client.getObject(Config.defaultBucketName, objID);
+                obj.close(); // 此处需要close
+            }
+            System.out.println("finish");
         } catch (Exception ex) {
             throw ex;
         }
